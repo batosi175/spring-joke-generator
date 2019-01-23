059d3c0591856741cf7c7b6f138698707ea9536e
Carlos Salazar <carlos.salazar@slalom.com>
2019-01-23 14:35:27 -0600

    updating to add java config

--- src/main/java/guru/springframework/jokegenerator/services/JokeService.java
+++ src/main/java/guru/springframework/jokegenerator/services/JokeService.java
@@ -1,8 +1,6 @@
 package guru.springframework.jokegenerator.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
 public interface JokeService {
 
     String getJoke();
--- src/main/java/guru/springframework/jokegenerator/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/jokegenerator/services/JokeServiceImpl.java
@@ -3,13 +3,16 @@ package guru.springframework.jokegenerator.services;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
+/**
+ * Created by jt on 5/25/17.
+ */
 @Service
-public class JokeServiceImpl implements JokeService{
+public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
